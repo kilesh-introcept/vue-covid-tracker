cf4566e8fecc899407521452644ad3cd651df1d6
Kilesh Maharjan <kilesh.maharjan@introcept.co>
2022-04-19 14:31:58 +0545

    adjustments

---
+++ src/Page/PageNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mx-auto">
+    <p>404 Page not Found</p>
+    <router-link to="/">Go Back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
--- src/Router/index.js
+++ src/Router/index.js
@@ -1,24 +1,43 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TodoPage from "../Page/todoPage.vue";
 import CovidTrackerPage from "../Page/CovidTracker.vue";
+import PageNotFound from "../Page/PageNotFound.vue";
 
 const routes = [
   {
     path: "/",
+    name: "Todo",
     component: TodoPage,
+    meta: {
+      title: "Todo List",
+    },
   },
   {
     path: "/covid",
+    name: "CovidTracker",
     component: CovidTrackerPage,
+    meta: {
+      title: "Covid Tracker",
+    },
   },
   {
     path: "/:pathMatch(.*)*",
-    redirect: "/covid",
+    name: "PageNotFound",
+    component: PageNotFound,
+    meta: {
+      title: "Page Not Found",
+    },
+    // redirect: "/covid",
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 export default router;
